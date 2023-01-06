3cf173037bb928cc4debfccd3189f01f17739c45
Andrei Dochkin <andreydochkin@yandex.ru>
2023-01-06 16:18:49 +0100

    refactor: change naming/ add new card popup

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,13 @@
-let page = document.querySelector(".page");
-let popup = page.querySelector(".popup");
-let editButton = page.querySelector(".profile__edit-button");
-let closeButton = page.querySelector(".popup__close-button");
+const page = document.querySelector(".page");
+
+const popupUser = page.querySelector(".user-popup");
+const popupAddCard = page.querySelector(".card-popup");
+
+const editButton = page.querySelector(".profile__edit-button");
+const addCardButton = page.querySelector(".profile__add-button");
+
+const closeUserButton = popupUser.querySelector(".popup__close-button");
+const closeAddCardButton = popupAddCard.querySelector(".popup__close-button");
 
 function openPopup(popup) {
   popup.classList.add("popup_opened");
@@ -11,29 +17,41 @@ function closePopup(popup) {
   popup.classList.remove("popup_opened");
 }
 
-function openEditProfilePopup(evt) {
+function openEditUserPopup(evt) {
   evt.preventDefault();
-  openPopup(popup);
+  openPopup(popupUser);
 }
 
-function closeEditProfilePopup(evt) {
+function openAddCardPopup(evt) {
   evt.preventDefault();
-  closePopup(popup);
+  openPopup(popupAddCard);
 }
 
-editButton.addEventListener("click", openEditProfilePopup);
-closeButton.addEventListener("click", closeEditProfilePopup);
+function closeEditUserPopup(evt) {
+  evt.preventDefault();
+  closePopup(popupUser);
+}
 
-let formElement = page.querySelector('.popup__form')
-let formInputs = formElement.querySelectorAll('input');
-let nameInput = formElement.querySelector('.popup__input_type_name');
-let jobInput = formElement.querySelector('.popup__input_type_about');
+function closeAddCardPopup(evt) {
+  evt.preventDefault();
+  closePopup(popupAddCard);
+}
 
-function handleFormSubmit (evt) {
+editButton.addEventListener("click", openEditUserPopup);
+addCardButton.addEventListener("click", openAddCardPopup);
+closeUserButton.addEventListener("click", closeEditUserPopup);
+closeAddCardButton.addEventListener("click", closeAddCardPopup);
+
+const userFormElement = popupUser.querySelector('.popup__form')
+const formInputs = userFormElement.querySelectorAll('input');
+const nameInput = userFormElement.querySelector('.popup__input_type_name');
+const jobInput = userFormElement.querySelector('.popup__input_type_about');
+
+function handleProfileFormSubmit (evt) {
     evt.preventDefault(); 
     page.querySelector('.profile__title').textContent = nameInput.value;
     page.querySelector('.profile__subtitle').textContent = jobInput.value;
-    closeEditProfilePopup(evt);
+    closeEditUserPopup(evt);
 }
 
-formElement.addEventListener('submit', handleFormSubmit);
+userFormElement.addEventListener('submit', handleProfileFormSubmit);
